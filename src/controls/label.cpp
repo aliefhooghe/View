74b4adc02424158a5dbd175259f2118f351707f1
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-03-25 18:45:36 +0100

    Make label better

--- src/controls/label.cpp
+++ src/controls/label.cpp
@@ -1,2 +1,36 @@
 
 #include "label.h"
+
+
+namespace View {
+
+    void label::apply_color_theme(const color_theme& theme)
+    {
+        _color = theme.on_surface;
+    }
+
+    void label::draw(cairo_t *cr)
+    {
+        set_source(cr, _color);
+        draw_text(cr, 0, 0, width(), height(), _font_size, _text.c_str(), _bold, _ha, _va);
+    }
+
+    void label::set_bold(bool bold) noexcept
+    {
+        _bold = bold;
+        invalidate();
+    }
+
+    void label::set_color(color c) noexcept
+    {
+        _color = c;
+        invalidate();
+    }
+
+    void label::set_font_size(float size) noexcept
+    {
+        _font_size = size;
+        invalidate();
+    }
+
+}
--- src/controls/label.h
+++ src/controls/label.h
@@ -11,30 +11,45 @@ namespace View {
     class label : public widget {
     public:
         template <typename Ttext>
-        label(float width, float height, Ttext&& str, float font_size, color c = 0x000000FFu, bool bold = false)
-        : widget{10, 10},
+        label(float width, float height, Ttext&& str, float font_size = 1.f, bool bold = false)
+        : widget{width, height},
             _text{std::forward<Ttext>(str)},
-            _bold{bold}, _color{c}, _font_size{font_size}
-        {}
+            _bold{bold}, _font_size{font_size},
+            _ha{horizontal_alignment::center},
+            _va{vertical_alignment::center}
+        {
+            apply_color_theme(default_color_theme);
+        }
+
+        template <typename Ttext>
+        label(Ttext&& str, float font_size = 1.f, bool bold = false)
+        : widget{1., font_size},
+            _text{std::forward<Ttext>(str)},
+            _bold{bold}, _font_size{font_size},
+            _ha{horizontal_alignment::left},
+            _va{vertical_alignment::top}
+        {
+        }
+
         ~label() override = default;
+
+        void apply_color_theme(const color_theme& theme) override;
 
         template <typename Ttext>
         void set_text(Ttext text) noexcept { _text = text; invalidate(); }
-        void set_bold(bool bold) noexcept { _bold = bold; invalidate(); }
-        void set_color(color c) noexcept { _color = c; invalidate(); }
-        void set_font_size(float size) noexcept { _font_size = size; invalidate(); }
 
-        void draw(cairo_t *cr) override
-        {
-            set_source(cr, _color);
-            draw_text(cr, 0, 0, width(), height(), _font_size, _text.c_str(), _bold);
-        }
+        void set_bold(bool bold) noexcept;
+        void set_color(color c) noexcept;
+        void set_font_size(float size) noexcept;
 
+        void draw(cairo_t *cr) override;
     private:
         std::string _text;
         bool _bold;
         color _color;
         float _font_size;
+        const horizontal_alignment _ha;
+        const vertical_alignment _va;
     };
 
 }
