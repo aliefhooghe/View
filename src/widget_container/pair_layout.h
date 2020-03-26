1a70aae2afe9d6c9ede5d57991999ad8e8775e69
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-03-26 23:33:07 +0100

    Refactor pair layout. Use coherent vertical/horizontal meaning

--- src/widget_container/pair_layout.h
+++ src/widget_container/pair_layout.h
@@ -6,9 +6,9 @@
 
 namespace View {
 
-    template <orientation Orientation /* Separator Orientation */>
+    template <orientation Orientation>
     class pair_layout : public panel_implementation<> {
-        static constexpr auto _separator_orthogonal_size = 4.0f;
+        static constexpr auto _separator_width = 1.f;
 
         /**
          * Orientation abstraction helpers
@@ -57,6 +57,13 @@ namespace View {
         }
 
         template <orientation O>
+        void resize_clamp(widget* p, float size)
+        {
+            const auto& size_constraint = get_size_constrain<O>(p);
+            resize<O>(p, size_constraint.clamp(size));
+        }
+
+        template <orientation O>
         void resize_clamp_delta(widget* p, float& delta)
         {
             const auto& size_constraint = get_size_constrain<O>(p);
@@ -67,17 +74,17 @@ namespace View {
         auto layout_width(widget* pleft, widget* pright)
         {
             if constexpr (Orientation == orientation::horizontal)
-                return std::max(pleft->width(), pright->width());
-            else
                 return pleft->width() + pright->width();
+            else
+                return std::max(pleft->width(), pright->width());
         }
 
         auto layout_height(widget* pleft, widget* pright)
         {
             if constexpr (Orientation == orientation::horizontal)
-                return pleft->height() + pright->height();
-            else
                 return std::max(pleft->height(), pright->height());
+            else
+                return pleft->height() + pright->height();
         }
 
         template <orientation O>
@@ -102,15 +109,15 @@ namespace View {
         pair_layout(std::unique_ptr<widget>&& first, std::unique_ptr<widget>&& second)
         : panel_implementation<>{layout_width(first.get(), second.get()), layout_height(first.get(), second.get())}
         {
-            //  Size along 'Orientation'
             const auto orientation_size = widget_size<Orientation>(this);
-            const auto first_width = widget_size<orthogonal(Orientation)>(first.get());
+            const auto orthogonal_size = widget_size<orthogonal(Orientation)>(this);
+            const auto first_orientation_size = widget_size<Orientation>(first.get());
 
-            resize<Orientation>(first.get(), orientation_size);
-            resize<Orientation>(second.get(), orientation_size);
+            resize<orthogonal(Orientation)>(first.get(), orthogonal_size);
+            resize<orthogonal(Orientation)>(second.get(), orthogonal_size);
 
             //  Compute and set layout size constraints
-            if constexpr (Orientation == orientation::horizontal) {
+            if constexpr (Orientation == orientation::vertical) {
                 set_size_constraints(
                     first->width_constraint().intersect(second->width_constraint()),
                     first->height_constraint() + second->height_constraint());
@@ -124,31 +131,31 @@ namespace View {
             //  Insert childrens
             insert_widget(0.f, 0.f, std::move(first));
             insert_widget(0.f, 0.f, std::move(second));
-            set_position<orthogonal(Orientation)>(_second(), first_width);
+            set_position<Orientation>(_second(), first_orientation_size);
 
             //Insert Separator
             auto separator =
-                std::make_unique<layout_separator>(0.f, 0.f, Orientation);
+                std::make_unique<layout_separator>(0.f, 0.f, orthogonal(Orientation));
 
             separator->set_callback(
                 [this](float delta) {
-                    const auto target_first_orthogonal_size = widget_size<orthogonal(Orientation)>(_first().get()) + delta;
-                    const auto target_second_orthogonal_size = widget_size<orthogonal(Orientation)>(_second().get()) - delta;
+                    const auto target_first_orientation_size = widget_size<Orientation>(_first().get()) + delta;
+                    const auto target_second_orientation_size = widget_size<Orientation>(_second().get()) - delta;
 
-                    if (match_size_constrain<orthogonal(Orientation)>(_first().get(), target_first_orthogonal_size) &&
-                        match_size_constrain<orthogonal(Orientation)>(_second().get(), target_second_orthogonal_size))
+                    if (match_size_constrain<Orientation>(_first().get(), target_first_orientation_size) &&
+                        match_size_constrain<Orientation>(_second().get(), target_second_orientation_size))
                     {
                         //  Move/Resize widgets
-                        resize<orthogonal(Orientation)>(_first().get(), target_first_orthogonal_size);
-                        resize<orthogonal(Orientation)>(_second().get(), target_second_orthogonal_size);
-                        set_position<orthogonal(Orientation)>(_second(), target_first_orthogonal_size);
+                        resize<Orientation>(_first().get(), target_first_orientation_size);
+                        resize<Orientation>(_second().get(), target_second_orientation_size);
+                        set_position<Orientation>(_second(), target_first_orientation_size);
 
                         //  Move Separator
-                        set_position<orthogonal(Orientation)>(
+                        set_position<Orientation>(
                             _separator(),
                             std::clamp(
-                                target_first_orthogonal_size - _separator_orthogonal_size / 2.f,
-                                0.f, widget_size<orthogonal(Orientation)>(this)));
+                                target_first_orientation_size - _separator_width / 2.f,
+                                0.f, widget_size<Orientation>(this)));
 
                         //  redraw
                         invalidate();
@@ -156,9 +163,9 @@ namespace View {
                 });
 
 
-            resize<Orientation>(separator.get(), orientation_size, _separator_orthogonal_size);
+            resize<Orientation>(separator.get(), _separator_width, orthogonal_size);
             insert_widget(0.f, 0.f, std::move(separator));
-            set_position<orthogonal(Orientation)>(_separator(), first_width - _separator_orthogonal_size / 2.f);
+            set_position<Orientation>(_separator(), first_orientation_size - _separator_width / 2.f);
         }
 
         ~pair_layout() override = default;
@@ -168,27 +175,27 @@ namespace View {
             if (width_constraint().contains(width) &&
                 height_constraint().contains(height))
             {
+                const auto current_orientation_size = widget_size<Orientation>(this);
                 const auto target_orientation_size = choose_dim<Orientation>(width, height);
                 const auto target_orthogonal_size = choose_dim<orthogonal(Orientation)>(width, height);
 
-                if (target_orthogonal_size != widget_size<orthogonal(Orientation)>(this)) {
-                    auto orthogonal_delta = target_orthogonal_size - widget_size<orthogonal(Orientation)>(this);
+                if (target_orientation_size != current_orientation_size) {
+                    auto orientation_delta = target_orientation_size - current_orientation_size;
 
                     //  Resize second as much as possible and then first
-                    resize_clamp_delta<orthogonal(Orientation)>(_second().get(), orthogonal_delta);
-                    resize_clamp_delta<orthogonal(Orientation)>(_first().get(), orthogonal_delta);
+                    resize_clamp_delta<Orientation>(_second().get(), orientation_delta);
+                    resize_clamp_delta<Orientation>(_first().get(), orientation_delta);
 
-                    //  Second and separator need to be moved
-                    const auto first_orthogonal_size = widget_size<orthogonal(Orientation)>(_first().get());
-                    set_position<orthogonal(Orientation)>(_second(), first_orthogonal_size);
-                    set_position<orthogonal(Orientation)>(_separator(), first_orthogonal_size - _separator_orthogonal_size / 2.f);
+                    //  Update second and separator position
+                    const auto first_orientation_size = widget_size<Orientation>(_first().get());
+                    set_position<Orientation>(_second(), first_orientation_size);
+                    set_position<Orientation>(_separator(), first_orientation_size - _separator_width / 2.f);
                 }
 
                 //  Resize along Orientation
-                resize<Orientation>(_first().get(), target_orientation_size);
-                resize<Orientation>(_second().get(), target_orientation_size);
-                resize<Orientation>(_separator().get(), target_orientation_size);
-
+                resize_clamp<orthogonal(Orientation)>(_first().get(), target_orthogonal_size);
+                resize_clamp<orthogonal(Orientation)>(_second().get(), target_orthogonal_size);
+                resize_clamp<orthogonal(Orientation)>(_separator().get(), target_orthogonal_size);
 
                 panel_implementation<>::resize(width, height);
 
@@ -209,9 +216,29 @@ namespace View {
      *  \brief Aliases for pair_layout
      *  \details A vertical layout have horizontal separator and vice versa
      **/
+    using horizontal_pair_layout = pair_layout<orientation::horizontal>;
+    using vertical_pair_layout = pair_layout<orientation::vertical>;
 
-    using vertical_pair_layout = pair_layout<orientation::horizontal>;
-    using horizontal_pair_layout = pair_layout<orientation::vertical>;
+
+    template <typename T, typename ...Ts>
+    auto make_vertical_layout(T&& first, Ts&& ...nexts)
+    {
+        if constexpr (sizeof...(Ts) == 0)
+            return std::move(first);
+        else
+            return std::make_unique<vertical_pair_layout>(
+                        std::move(first), make_vertical_layout(std::move(nexts)...));
+    }
+
+    template <typename T, typename ...Ts>
+    auto make_horizontal_layout(T&& first, Ts&& ...nexts)
+    {
+        if constexpr (sizeof...(Ts) == 0)
+            return std::move(first);
+        else
+            return std::make_unique<horizontal_pair_layout>(
+                        std::move(first), make_horizontal_layout(std::move(nexts)...));
+    }
 
 }
 
